9c9cde3b88730c971d360e8ad8298323756bbf3e
Alexander Keth <alexander.keth@uni-hamburg.de>
2020-12-04 10:18:12 +0100

    result quiz week 4 q 4.

--- quiz-week-4-4.R
+++ quiz-week-4-4.R
@@ -1,6 +1,7 @@
 library(lubridate) # For year() function below
+library(forecast)
 
-dat = read.csv("~/Desktop/gaData.csv")
+dat = read.csv("C:/~/practical-machine-learning/data/gaData.csv")
 
 training = dat[year(dat$date) < 2012,]
 
@@ -9,7 +10,19 @@ testing = dat[(year(dat$date)) > 2011,]
 tstrain = ts(training$visitsTumblr)
 
 # Fit a model using the bats() function in the forecast package to the training time series.
+mod <- bats(tstrain)
 
-# Then forecast this model for the remaining time points.
+# Then forecast this model for the remaining time
+tstest <- ts(testing$visitsTumblr)
+
+pred <- forecast(mod, h = length(tstest))
 
 # For how many of the testing points is the true value within the 95% prediction interval bounds?
+pred$upper[, 2]
+pred$lower[, 2]
+
+str(tstest)
+
+mean(as.numeric(tstest) >= pred$lower[, 2] & as.numeric(tstest) <= pred$upper[, 2])
+
+# 0.9617021
