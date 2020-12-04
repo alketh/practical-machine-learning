ef5c69b529e65e754db0c27b73df94cdcf587529
Alexander Keth <alexander.keth@uni-hamburg.de>
2020-12-04 10:44:31 +0100

    Difference due to legacy packages.

--- quiz-week-4-5.R
+++ quiz-week-4-5.R
@@ -1,6 +1,8 @@
 set.seed(3523)
 
 library(AppliedPredictiveModeling)
+library(caret)
+library(e1071)
 
 data(concrete)
 
@@ -8,9 +10,22 @@ inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
 
 training = concrete[ inTrain,]
 
-testin
+testing = concrete[-inTrain, ]
 
 # Set the seed to 325 and fit a support vector machine using the e1071 package
 # to predict Compressive Strength using the default settings.
 
+set.seed(325)
+
+mod <- svm(CompressiveStrength~ ., data = training)
+
 # Predict on the testing set. What is the RMSE?
+pred <- predict(mod, testing)
+
+sqrt(mean((testing$CompressiveStrength - pred)^2))
+
+accuracy(pred, testing$CompressiveStrength)
+
+# difference due to package versions
+# my answer 7.962075
+# correct answer 6.72
