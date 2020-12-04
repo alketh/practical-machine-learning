5adc2a4029ac665614fe366a5787239824a38ec4
Alexander Keth <alexander.keth@uni-hamburg.de>
2020-12-04 10:44:15 +0100

    Add in elasticnet package.

--- quiz-week-4-3.R
+++ quiz-week-4-3.R
@@ -1,4 +1,5 @@
 library(AppliedPredictiveModeling)
+library(elasticnet)
 
 set.seed(3523)
 
@@ -15,7 +16,8 @@ testing = concrete[-inTrain,]
 set.seed(233)
 
 # fit a lasso model to predict Compressive Strength.
-
+modFit <- train(CompressiveStrength ~ ., method = "lasso", data = training)
 
 # Which variable is the last coefficient to be set to zero as the penalty increases?
 # (Hint: it may be useful to look up ?plot.enet).
+plot.enet(modFit$finalModel, xvar = "penalty", use.color = TRUE)
